e9d2b82e598865f8f62c0b305369eb27dac28d04
Sanjay <109132753+sanjay9751600684@users.noreply.github.com>
2023-09-04 12:50:34 +0530

    Add files via upload

---
+++ src/demo.js
@@ -0,0 +1,42 @@
+// import * as React from 'react';
+// import { styled } from '@mui/material/styles';
+// import Card from '@mui/material/Card';
+// import CardHeader from '@mui/material/CardHeader';
+// import CardMedia from '@mui/material/CardMedia';
+// import CardContent from '@mui/material/CardContent';
+
+// import Avatar from '@mui/material/Avatar';
+// import IconButton from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+// import { red } from '@mui/material/colors';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+// const ExpandMore = styled((props) => {
+//   const { expand, ...other } = props;
+
+
+
+//   return <IconButton {...other} />;
+// })(({ theme, expand }) => ({
+//   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+//   marginLeft: 'auto',
+//   transition: theme.transitions.create('transform', {
+//     duration: theme.transitions.duration.shortest,
+//   }),
+// }));
+
+// export default function RecipeReviewCard() {
+//   const [expanded, setExpanded] = React.useState(false);
+
+//   const handleExpandClick = () => {
+//     setExpanded(!expanded);
+//   };
+//   const Coin = ({image, name, price, volume, symbol, priceChange,marketcap}) => {
+
+//   return (
+//     <Card sx={{ maxWidth: 345 }}>
+// efwf      
+//     </Card>
+//   );
+//   }
+//     }
